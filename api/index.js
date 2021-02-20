2485cda2e2bdac34dc5f7d760c35dbe0c2de2af2
davide mezzetti <davide.mezzetti@hotmail.it>
2021-02-20 21:11:06 +0100

    created dummy data

--- api/index.js
+++ api/index.js
@@ -13,41 +13,67 @@ app.use(bodyParser.json())
 
 const _CONTENT =
   "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-const data = [
+const users = [
   {
     id: 1,
-    title: 'Davide',
-    content: _CONTENT,
-    like: { count: 340, data: [{ id: 4, name: 'Grace' }] }
+    nome: 'Davide',
+    password: 'password',
+    role: 'Developer'
   },
   {
     id: 2,
-    title: 'Django',
+    nome: 'Grace',
+    password: 'password',
+    role: 'Developer'
+  }
+]
+let likes = [
+  {
+    NoteId: 1,
+    AuthorId: 2
+  },
+  {
+    NoteId: 1,
+    AuthorId: 1
+  },
+  {
+    NoteId: 3,
+    AuthorId: 1
+  }
+]
+
+let data = [
+  {
+    id: 1,
+    title: 'This is a post about software',
     content: _CONTENT,
-    like: {
-      count: 2,
-      data: [
-        { id: 4, name: 'Grace' },
-        { id: 2, name: 'Pinco' }
-      ]
-    }
+    AuthorId: 1
+  },
+  {
+    id: 2,
+    title: 'This is a post about Django',
+    content: _CONTENT,
+    AuthorId: 1
   },
   {
     id: 3,
-    title: 'Pinco',
+    title: 'This is a post about nature',
     content: _CONTENT,
-    like: { count: 0, data: [] }
-  },
-  {
-    id: 4,
-    title: 'Grace',
-    content: 'ciao',
-    like: { count: 1, data: [{ id: 4, name: 'Grace' }] }
+    AuthorId: 2
   }
 ]
+
+app.post('/like', (req, res) => {
+  const { PostId } = req.body
+  //get author id
+
+  res.status(201)
+  res.send()
+})
+
 app.get('/post', (req, res) => {
   //console.log('OK')
-
+  res.status(200)
   res.send(data)
 })
 
@@ -68,6 +94,43 @@ app.post('/post', (req, res) => {
   res.send(newPost)
 })
 
+/* update a post **/
+app.put('/post/:id', (req, res) => {
+  //console.log('OK')
+  console.log(req.params)
+  const { title, content } = req.body
+  let updatedPost = null
+  data = data.map(post => {
+    if (post.id === req.params.id) {
+      updatedPost = {
+        ...post,
+        title,
+        content,
+        updatedAt: new Date()
+      }
+      return updatedPost
+    }
+    return post
+  })
+
+  res.status(200)
+  res.send(updatedPost)
+})
+
+/* update a post **/
+app.delete('/post/:id', (req, res) => {
+  //console.log('OK')
+
+  data = data.filter(post => {
+    if (post.id !== req.params.id) {
+      return post
+    }
+  })
+
+  res.status(200)
+  res.send()
+})
+
 app.get('/post/:id', (req, res) => {
   //console.log('OK')
   console.log(req.body)
@@ -85,22 +148,6 @@ app.get('/post/:id', (req, res) => {
   res.send(newPost)
 })
 
-const users = [
-  {
-    id: 1,
-    nome: 'Davide',
-    password: 'password',
-    role: 'Developer',
-    isAuth: false
-  },
-  {
-    id: 2,
-    nome: 'Grace',
-    password: 'password',
-    role: 'Developer',
-    isAuth: false
-  }
-]
 app.post('/login', (req, res) => {
   try {
     const { username, password } = req.body
